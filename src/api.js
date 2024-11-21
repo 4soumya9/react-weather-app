8c4814d2c19153d639707d7c85c55d6a2a35c992
Soumyajit <4soumya19@gmail.com>
2024-11-21 23:22:39 +0530

    First Phase

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "e3b34f5146mshde87fdbd54f3022p1b0ec9jsna1f8e6cb0eb8", // enter your rapid api key here
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./current-weather.css";
+const CurrentWeather = () => {
+  return (
+    <div className='weather'>
+        <div className="top">
+          <p className='city'>London</p>
+          <p></p>
+        </div>
+    </div>
+  )
+}
+
+export default  CurrentWeather
